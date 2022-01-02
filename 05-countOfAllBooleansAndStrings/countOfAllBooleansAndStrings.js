30dbebca0f6b60b5e77beebeec008b257e9d912c
dangusernamenottaken <96688208+dangusernamenottaken@users.noreply.github.com>
2022-01-02 05:06:26 -0500

    commit 2

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -2,7 +2,7 @@ function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
   
    let count = 0;
-  for(let i = 0; i < arr.lenghth; i++){
+  for(let i = 0; i < arr.length; i++){
     if(arr[i] == true || arr[i] == false || arr[i] instanceof String){
        count++;
     }
