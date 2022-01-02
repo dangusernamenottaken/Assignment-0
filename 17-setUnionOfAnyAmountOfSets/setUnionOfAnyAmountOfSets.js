88bdf56158c0df7c4f6458cbbf3fab1d3f08a819
dangusernamenottaken <96688208+dangusernamenottaken@users.noreply.github.com>
2022-01-02 14:36:30 -0500

    Commit 1

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,13 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+   
+  let argsArray = Array.from(args);
+  let newSet = new Set(argsArray);
+  
+  return newSet;
+  
+  
+
 }
 
 // Do not edit this line;
