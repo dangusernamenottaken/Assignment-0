9f6c08c6b6e0bca0c176ef40c1a271ac5bde3d1a
dangusernamenottaken <96688208+dangusernamenottaken@users.noreply.github.com>
2022-01-02 06:05:03 -0500

    commit 2

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -8,6 +8,17 @@ function countOfAllIndexMatchingNumbers(nums) {
      }
     }
   }
+  for(let i = 0; i < nums.length; i++){
+   let numberFunction = num => Number(num);
+    let strInt = Array.from(String(nums[i]),numberFunction);
+    for(let j = 0; j < strInt.length; j++){
+     for(let k = j+1; k <  strInt.length; k++){
+         if(strInt[j]==strInt[k]{
+            count++;
+            }
+     }    
+    }
+  }
   if(count == 0){
     return nums.length;
   }
