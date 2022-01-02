fbd0a1bd35002662cc64e7e843137c2a340dab6b
dangusernamenottaken <96688208+dangusernamenottaken@users.noreply.github.com>
2022-01-02 12:39:58 -0500

    Commit 1

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,24 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  let arrFizzBuzz = [];
+  let arrLength = (end - start) + 1;
+  for(let i = 0; i < arrLength; i++){
+      arrFizzBuzz[i] = i+1;
+    if(arrFizzBuzz[i]%3 == 0 && arrFizzBuzz[i]%5 == 0){
+       arrFizzBuzz[i] = "FizzBuzz";
+      
+    }
+    else if(arrFizzBuzz[i]%3 == 0){
+      arrFizzBuzz[i] = "Fizz";
+      
+    }
+    
+    else if(arrFizzBuzz[i]%5== 0){
+       arrFizzBuzz[i] = "Buzz";
+      
+    }
+  }
+  return arrFizzBuzz;
 }
 
 // Do not edit this line;
