7cb462ca2ae8ecb84866fcb6110095171c74e692
dangusernamenottaken <96688208+dangusernamenottaken@users.noreply.github.com>
2022-01-02 12:04:00 -0500

    commit 1

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,20 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  let max = 0;
+  let min = 0;
+  let sum = 0;
+  for(let i = 0; i < nums.length; i++){
+   for(let j = i+1; j < nums.length; j++){
+    if(nums[i]<=nums[j]){
+       min = nums[i]; 
+    }
+    if(nums[i]>=nums[j]){
+       max = nums[i];
+    }
+   }
+  }
+  sum = max + min;
+  return sum;
 }
 
 // Do not edit this line;
