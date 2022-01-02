404187ff35757a035cd8f7c37d3f805f52495fec
dangusernamenottaken <96688208+dangusernamenottaken@users.noreply.github.com>
2022-01-01 23:54:40 -0500

    commit 1

--- 03-cutInHalfAndFloor/cutInHalfAndFloor.js
+++ 03-cutInHalfAndFloor/cutInHalfAndFloor.js
@@ -1,5 +1,13 @@
 function cutInHalfAndFloor(num) {
   // Insert code here;
+  let cut;
+  if( num % 2 == 1){
+   cut = (num -1) / 2;
+  }
+  else{
+    cut = num/2;
+  }
+  return cut;
 }
 
 // Do not edit this line;
