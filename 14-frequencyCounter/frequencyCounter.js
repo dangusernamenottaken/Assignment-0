ce5aaa917560750602f3ccb463781d36408e65fb
dangusernamenottaken <96688208+dangusernamenottaken@users.noreply.github.com>
2022-01-02 13:18:27 -0500

    Commit 1

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,17 @@
 function frequencyCounter(word) {
   // Insert code here;
+  let mapp = new Map();
+  for(let i = 0; i < word.length; i++){
+   let letter = string.charAt(i); 
+    if(!(letter in mapp)){
+      mapp.set(letter,1);
+    }
+    if(letter in mapp){
+     mapp.set(letter,(mapp.get(letter))+1); 
+      
+    }
+  }
+  return mapp;
 }
 
 // Do not edit this line;
