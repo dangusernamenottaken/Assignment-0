da343aaf88cbadb00947ef85c63f8c14753f80fd
dangusernamenottaken <96688208+dangusernamenottaken@users.noreply.github.com>
2022-01-01 23:41:06 -0500

    commit 1

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,13 @@
 function titleCaseEdit(title) {
   // Insert code here;
-  console.log(title);
+  title = title.toLowerCase();
+  title = title.split(' ');
+  for(let i = 0; i< title.length; i++){
+      title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1);
+  
+  }
+  
+return title.join(' ');
 }
 
 // Do not edit this line;
