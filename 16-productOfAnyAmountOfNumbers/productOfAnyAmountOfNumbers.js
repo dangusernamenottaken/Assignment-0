a6d355a34a6aba6d1607f3868db0ba5432a8de15
dangusernamenottaken <96688208+dangusernamenottaken@users.noreply.github.com>
2022-01-02 14:36:56 -0500

    Commit 2

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -2,7 +2,7 @@ function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
   let product = 1;
   let argsArray = Array.from(args);
-  for(let i = 0; i < args.length; i++){
+  for(let i = 0; i < argsArray.length; i++){
       product*= args[i];
   }
   return product;
