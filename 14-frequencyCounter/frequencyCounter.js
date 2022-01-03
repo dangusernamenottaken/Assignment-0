7dd64e47cfb17d00997314c3485d89e9a44538f5
dangusernamenottaken <96688208+dangusernamenottaken@users.noreply.github.com>
2022-01-03 14:32:43 -0500

    Commit 3

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,22 @@
+//create empty set - done
+// loop word - done
+//make an var - done
+// if letter in set, increment, else add -done
+// return object - done
+
 function frequencyCounter(word) {
   // Insert code here;
+  // var frequency = {};
+  // for(var i = 0; i < word.length; i++){
+  // let letter = word.charAt(i);
+  // if(frequency[letter]){
+  // frequence[letter]++;
+  // else{
+  // frequence[letter] = 1;
+  // }
+  //}
+  // return frequency;
+  //}
   let mapp = new Map();
   for(let i = 0; i < word.length; i++){
    let letter = string.charAt(i); 
