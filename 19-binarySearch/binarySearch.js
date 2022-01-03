479085a282abee9e6d43a42e19164aa3bb2da393
dangusernamenottaken <96688208+dangusernamenottaken@users.noreply.github.com>
2022-01-03 01:24:43 -0500

    Commit 1

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,21 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+   let start = 0;
+   let end = nums.length-1;
+    while(start <= end){
+     let middle = Math.floor((start+end)/2);
+      if(nums[middle]==target){
+         return true; 
+      }
+      else if(nums[middle]<target){
+        start = middle + 1;
+      }
+      
+      else{
+        end = middle - 1;
+      }
+    }
   }
 }
 
