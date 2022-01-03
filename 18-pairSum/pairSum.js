c2649d2e3f8e6c049564af23c027c633f407c709
dangusernamenottaken <96688208+dangusernamenottaken@users.noreply.github.com>
2022-01-02 21:10:14 -0500

    Commit 1

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,15 @@
 function pairSum(nums, target) {
   // Insert code here;
+  for(let i =0; i < nums.length; i++){
+   for(let j = i+1; j < nums.length; j++){
+     if((nums[i]+nums[j]) == target){
+      return true; 
+     }
+     else {
+       return false;
+     }
+   }
+  }
 }
 
 // Do not edit this line;
