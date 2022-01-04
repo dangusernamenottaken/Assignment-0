0621803093c3721a091fcf06bdb44e304b55b0de
dangusernamenottaken <daniel.guberman@cix.csi.cuny.edu>
2022-01-03 21:21:30 -0500

    commiting

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,20 +1,8 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
-  let max = 0;
-  let min = 0;
-  let sum = 0;
-  for(let i = 0; i < nums.length; i++){
-   for(let j = i+1; j < nums.length; j++){
-    if(nums[i]<=nums[j]){
-       min = nums[i]; 
-    }
-    if(nums[i]>=nums[j]){
-       max = nums[i];
-    }
-   }
-  }
-  sum = max + min;
-  return sum;
+let sum = 0;
+sum = nums[0] + nums[nums.length-1];
+return sum;
 }
 
 // Do not edit this line;
