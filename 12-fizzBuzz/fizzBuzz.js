d63d4f961461dffb05f6475e63ed26da6faef399
dangusernamenottaken <daniel.guberman@cix.csi.cuny.edu>
2022-01-03 22:19:41 -0500

    fix code

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,23 +1,30 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+
   let arrFizzBuzz = [];
-  let arrLength = (end - start) + 1;
+  let arrLength = (end - start)+1;
+  let count = start;
   for(let i = 0; i < arrLength; i++){
-      arrFizzBuzz[i] = i+1;
-    if(arrFizzBuzz[i]%3 == 0 && arrFizzBuzz[i]%5 == 0){
+      //count = start;
+    arrFizzBuzz[i] = count;
+    count++;
+     
+      
+    if(arrFizzBuzz[i]%15 === 0){
        arrFizzBuzz[i] = "FizzBuzz";
       
     }
-    else if(arrFizzBuzz[i]%3 == 0){
+    else if((arrFizzBuzz[i]%3) == 0){
       arrFizzBuzz[i] = "Fizz";
       
     }
     
-    else if(arrFizzBuzz[i]%5== 0){
+    else if((arrFizzBuzz[i]%5)== 0 ){
        arrFizzBuzz[i] = "Buzz";
       
     }
-  }
+    
+      }
   return arrFizzBuzz;
 }
 
